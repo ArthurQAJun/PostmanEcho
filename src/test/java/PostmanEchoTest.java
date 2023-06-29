cd2ff0601875f864e5be2c003b39d0c1ec82d864
Arthur <rutra.ru@yandex.ru>
2023-06-29 22:32:12 +0500

    Failed Test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some Data"))
         ;
     }
 }
